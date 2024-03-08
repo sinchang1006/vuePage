8b11316020e705783072ace32d143f6fead9da64
DESKTOP-AIOR3RP\황인섭 <ishwang@tront.co.kr>
2024-03-08 14:16:20 +0900

    배포 테스트

---
+++ docs/assets/TestPageView-Ba7tHr7b.js
@@ -0,0 +1,1 @@
+const a={__name:"TestPageView",setup(e){return(t,_)=>" TEST PAGE "}};export{a as default};
---
+++ vue-page/src/views/TestPageView.vue
@@ -0,0 +1,11 @@
+<template>
+    TEST PAGE 
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>
+</style>
